d4d81f959639c6a9811d1ef7282f4c57ee7b8dbb
James Bowman <James Bowman>
2020-12-25 16:37:11 -0500

    FTDI port images

--- samples/dazzler-color1.ino
+++ samples/dazzler-color1.ino
@@ -1,22 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
   int attr, fg, bg;
-
-  printf(
-   "Dazzler listens on the serial line and emulates a "
+  printf( "Dazzler listens on the serial line and emulates a "
    "terminal, generating\noutput on a standard HDMI connector."
    "It gives embedded microcontrollers\na real console.\n\n");
-  for (attr = 0; attr <= 1; attr++) {
+  for (attr = 0; attr <= 1; attr++)
     for (fg = 30; fg <= 37; fg++) {
-      for (bg = 40; bg <= 47; bg++) {
+      for (bg = 40; bg <= 47; bg++)
         printf("\e[%d;%d;%dm%d;%d;%d\e[m",
-               attr, bg, fg, attr, bg, fg);
-      }
+          attr, bg, fg, attr, bg, fg);
       printf("\n");
     }
-  }
-  
   return 0;
 }
